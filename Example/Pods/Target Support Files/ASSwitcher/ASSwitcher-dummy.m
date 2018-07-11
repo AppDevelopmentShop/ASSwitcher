ab3993515cddfcc7fa85534292833cb17b9ef6fd
mvolkov-42 <maxvolkov.inc@gmail.com>
2018-07-11 15:45:57 +0300

    add example project

---
+++ Example/Pods/Target Support Files/ASSwitcher/ASSwitcher-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ASSwitcher : NSObject
+@end
+@implementation PodsDummy_ASSwitcher
+@end
---
+++ Example/Pods/Target Support Files/Pods-ASSwitcher_Tests/Pods-ASSwitcher_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ASSwitcher_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ASSwitcher_Tests
+@end
